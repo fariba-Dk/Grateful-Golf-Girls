5fdb6e36fbe252a4c2d2c6dca3c138b0d7f307bb
Fariba Dadko <faribacodes@gmail.com>
2022-04-05 22:57:26 -0700

    adjusted color

--- src/themes.js
+++ src/themes.js
@@ -1,10 +1,10 @@
 export const darkTheme = {
   body: "#ffaaa7",
-  elevation_1: "#83ffe6",
+  elevation_1: "#22eaca",
   elevation_2: "#black",
   elevation_3: "#black",
   text: "#1a0841",
-  primary: "#01a9b4",
+  primary: "#b31e6f",
   primaryHover: "#492b7b",
   shadow: "0 2px 5px 1px #111",
 };
